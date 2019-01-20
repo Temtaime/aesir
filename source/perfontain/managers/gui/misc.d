bb7c6e6dd294893ebca27dcae522a9a013c5bd24
Temtaime <temtaime@gmail.com>
2019-01-20 23:41:28 +0300

    Add new missed file

---
+++ source/perfontain/managers/gui/misc.d
@@ -0,0 +1,39 @@
+module perfontain.managers.gui.misc;
+
+import
+		std.experimental.all,
+
+		perfontain;
+
+
+template MakeChildRef(T, string Name, Idx...)
+{
+	mixin(`inout(T) ` ~ Name ~ `() inout
+	{
+		GUIElement e = cast()this;
+
+		foreach(i; Idx)
+		{
+			if(i >= e.childs.length)
+			{
+				return null;
+			}
+
+			e = e.childs[i];
+		}
+
+		return cast(inout(T))e;
+	}`);
+}
+
+auto calcSize(in GUIElement[] arr, bool withPos = true)
+{
+	Vector2s res;
+
+	foreach(i; 0..2)
+	{
+		res[i] = cast(short)arr.map!(a => withPos ? a.end[i] : a.size[i]).fold!max(0);
+	}
+
+	return res;
+}
