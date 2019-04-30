67415354d751dc5b9767c6f6b6c694bc455dd4c3
Temtaime <temtaime@gmail.com>
2019-04-30 16:17:09 +0300

    Refactor inventory and storage

--- source/rocl/controls/inventory.d
+++ source/rocl/controls/inventory.d
@@ -29,33 +29,36 @@ final class InventoryTab : GUIElement
 	{
 		super(p);
 
+		{
+			auto msgs =
+			[
+				MSG_ITM,
+				MSG_EQP,
+				MSG_ETC
+			];
+
+			new TextTabs(this, msgs);
+
+			foreach(u; tt.tabs)
+			{
+				auto sc = new Scrolled(u, sz, 36);
+				sc.size.x += 36 * sz.x;
+			}
+
+			tt.adjust;
+		}
+
 		foreach(i; 0..sz.x)
 		foreach(j; 0..sz.y)
 		{
 			auto im = new GUIImage(this, INV_ITEM);
 
-			//im.flags.background = true;
-			im.pos = Vector2s(INV_TAB_ITEM_SZ.x, 0) + Vector2s(i * 36, j * 36) + (Vector2s(36) - im.size) / 2;
+			im.pos = tt.tabs[0].pos;
+			im.pos += Vector2s(i, j) * 36 + (Vector2s(36) - im.size) / 2;
 		}
 
-		auto w = sz.x * 36 + SCROLL_ARROW_SZ.x;
-		auto m = sz.y > 2;
-
-		size = Vector2s(INV_TAB_ITEM_SZ.x + w, sz.y * 36);
-
-		{
-			auto n = new TabWindow(this, cast(ushort)w, [ 24, 27, 29 ], INV_TAB_ITEM, m ? size.y : -1);
-
-			foreach(i; 0..3)
-			{
-				//new Scrolled(n.tabs[i], Vector2s(n.tab.size.x, 36), sz.y);
-			}
-
-			if(!m)
-			{
-				size.y = n.size.y;
-			}
-		}
+		tt.bringToTop;
+		toChildSize;
 	}
 
 	void register(Item m)
@@ -75,175 +78,49 @@ final class InventoryTab : GUIElement
 	}
 
 private:
+	auto scOf(ubyte tab)
+	{
+		return cast(Scrolled)tt.tabs[tab].childs[0];
+	}
+
 	void add(Item m)
 	{
-		//add(_aa[m] = new ItemHolder(null, m), m.tab);
+		scOf(m.tab).add(_aa[m] = new ItemHolder(m));
 	}
 
 	void remove(Item m)
 	{
 		if(auto p = m in _aa)
 		{
-			remove(*p, m.tab);
+			scOf(m.tab).remove(*p);
 			_aa.remove(m);
 		}
 	}
 
-	void remove(ItemHolder m, ubyte tab)
-	{
-		auto sc = scOf(tab);
-		auto e = m.parent;
-
-		/*if(e is sc.rows.back)
-		{
-			if(e.childs.length == 1)
-			{
-				sc.remove(e);
-			}
-			else
-			{
-				auto s = e.childs[].countUntil!(a => a is m);
-
-				e.childs.remove(m);
-				e.childs[s..$].each!(a => a.pos.x -= 36);
-			}
-		}
-		else
-		{
-			RCArray!ItemHolder arr;
-
-			auto idx = sc.rows[].countUntil!(a => a is e) + 1;
-			assert(idx > 0);
-
-			while(idx != sc.rows.length)
-			{
-				auto r = sc.rows[idx];
-
-				foreach(w; r.childs)
-				{
-					arr ~= cast(ItemHolder)w;
-				}
-
-				sc.remove(r);
-			}
-
-			remove(m, tab);
-			arr.each!(a => add(a, tab));
-		}*/
-	}
-
-	void add(ItemHolder m, ubyte tab)
-	{
-		/*GUIElement c;
-
-		{
-			auto sc = scOf(tab);
-
-			{
-				auto rs = sc.rows[];
-
-				if(!rs.length || (c = rs.back).childs.length == 7)
-				{
-					c = new GUIElement(null);
-					c.size = Vector2s(36 * 7, 36);
-
-					//sc.add(c, true);
-				}
-			}
-		}
-
-		m.parent = c;
-		m.pos = Vector2s(c.childs.length * 36, 0);
-
-		c.childs ~= m;*/
-	}
-
-	auto scOf(ubyte idx)
-	{
-		return cast(Scrolled)tabWin
-									.tabs[idx]
-									.childs
-									.front;
-	}
-
-	inout tabWin()
-	{
-		return cast(TabWindow)childs.back;
-	}
-
 	ItemHolder[Item] _aa;
+	mixin MakeChildRef!(TextTabs, `tt`, 0);
 }
 
-final class WinStorage : WinBasic
+final class WinStorage : WinBasic2
 {
 	this()
 	{
-		name = `storage`;
+		super(MSG_STORAGE, `storage`);
 
-		auto sz = Vector2s(7, 4);
-
-		{
-			tab = new InventoryTab(this, sz);
-			tab.pos = Vector2s(2) + Vector2s(0, WIN_TOP_SZ.y);
-
-			sz = tab.pos + tab.size + Vector2s(0, WIN_BOTTOM_SZ.y) + Vector2s(2) + Vector2s(0, sz.y <= 2 ? -10 : 0);
-
-			super(sz, MSG_STORAGE);
-		}
+		auto tab = new InventoryTab(main, Vector2s(7, 4));
+		adjust;
 
 		{
-			auto b = new Button(this, MSG_CLOSE);
+			auto b = new Button(bottom, MSG_CLOSE);
 
-			b.pos = size - Vector2s(b.size.x + 4, (WIN_BOTTOM_SZ.y + b.size.y) / 2);
+			b.move(POS_MIN, 5, POS_CENTER);
 			b.onClick = () => ROnet.storeClose;
 		}
 
-		items.onAdded.permanent(&onAdd);
+		items.onAdded.permanent(&tab.register);
 	}
 
 	Items items;
-	InventoryTab tab;
-private:
-	void onAdd(Item m)
-	{
-		m.source = ITEM_STORAGE;
-		tab.register(m);
-	}
-}
-
-/*final class TabSelector : GUIElement
-{
-	this(GUIElement p, string[] arr)
-	{
-		super(p);
-
-		_texs = arr.map!(a => new GUIStaticText(null, a)).array;
-
-	}
-
-private:
-	GUIStaticText[] _texs;
-}*/
-
-class InventoryTab2 : GUIElement
-{
-	this(GUIElement p, Vector2s sz)
-	{
-		super(p);
-
-		new Scrolled(this, sz, 36);
-		sc.size.x += 36 * sz.x;
-
-		toChildSize;
-	}
-
-	void add(Item m)
-	{
-		sc.add(new ItemHolder(m));
-	}
-
-private:
-	mixin MakeChildRef!(Scrolled, `sc`, 0);
 }
 
 final class WinInventory : WinBasic2
@@ -252,58 +129,11 @@ final class WinInventory : WinBasic2
 	{
 		super(MSG_INVENTORY, `inventory`);
 
-		auto sz = Vector2s(7, 2);
-
-		{
-			auto msgs =
-			[
-				MSG_ITM,
-				MSG_EQP,
-				MSG_ETC
-			];
-
-			auto e = new TextTabs(main, msgs);
-
-			foreach(i, u; e.tabs)
-			{
-				new InventoryTab2(u, sz);
-			}
-
-			foreach(i; 0..sz.x)
-			foreach(j; 0..sz.y)
-			{
-				auto im = new GUIImage(main, INV_ITEM);
-
-				im.pos = Vector2s(i, j) * 36 + (Vector2s(36) - im.size) / 2;
-				im.pos += e.tabs[0].pos;
-			}
-
-			e.adjust;
-			e.bringToTop;
-
-			RO.status.items.onAdded.permanent(a => (cast(InventoryTab2)e.tabs[0].childs[0]).add(a));
-		}
-
+		new InventoryTab(main, Vector2s(7, 2));
 		adjust;
 
-		/*auto sz = Vector2s(7, 2);
-
-		{
-			tab = new InventoryTab(this, sz);
-			tab.pos = Vector2s(2) + Vector2s(0, WIN_TOP_SZ.y);
-
-			sz = tab.pos + tab.size + Vector2s(0, WIN_BOTTOM_SZ.y) + Vector2s(2) + Vector2s(0, sz.y <= 2 ? -10 : 0);
-		}
-
-		if(pos.x < 0)
-		{
-			pos = Vector2s(0, RO.gui.base.size.y);
-		}
-
-		RO.status.items.onAdded.permanent(&tab.register);*/
+		RO.status.items.onAdded.permanent(&tab.register);
 	}
-
-	//InventoryTab tab;
 
 	mixin StatusValue!(uint, `zeny`, onUpdate);
 	mixin StatusValue!(uint, `weight`, onUpdate);
@@ -311,14 +141,13 @@ final class WinInventory : WinBasic2
 private:
 	void onUpdate()
 	{
-		/*if(childs.length > 2)
-		{
-			childs.popBack;
-		}
+		bottom.childs.clear;
 
-		auto e = new GUIStaticText(this, format(`%s Ƶ, %s: %u / %u`, price(zeny), MSG_WEIGHT, weight, maxWeight));
-		e.pos = Vector2s(tab.pos.x + INV_TAB_ITEM_SZ.x, size.y - (WIN_BOTTOM_SZ.y + e.size.y) / 2);*/
+		auto e = new GUIStaticText(bottom, format(`%s Ƶ, %s: %u / %u`, price(zeny), MSG_WEIGHT, weight, maxWeight));
+		e.move(POS_MIN, 5, POS_CENTER);
 	}
+
+	mixin MakeChildRef!(InventoryTab, `tab`, 0);
 }
 
 final class ItemHolder : GUIElement
@@ -338,7 +167,7 @@ final class ItemHolder : GUIElement
 private:
 	void recount(Item m)
 	{
-		while(childs.length > 1)
+		if(childs.length > 1)
 		{
 			childs.popBack;
 		}
