9e740ee163643f28f43849883322ccf71230d6c7
Temtaime <temtaime@gmail.com>
2019-11-02 02:11:52 +0300

    Add manifest for better appearance

--- source/app.d
+++ source/app.d
@@ -11,6 +11,11 @@ import
 		rocl.game;
 
 
+version(Windows)
+{
+	pragma(linkerDirective, `"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'"`);
+}
+
 extern(C) __gshared
 {
 	bool rt_cmdline_enabled = false;
