a94b30b7c27d40657cb2bb4ec4733a752cf55a81
Temtaime <temtaime@gmail.com>
2020-11-22 23:21:42 +0300

    WIP

--- source/rocl/controls/status/package.d
+++ source/rocl/controls/status/package.d
@@ -18,7 +18,7 @@ final class WinStatus : GUIWindow
 				Slot(EQP_ACC_R, MSG_ACC), Slot(EQP_ACC_L, MSG_ACC)
 			];
 
-			auto r = cast(uint) ctx.style.combo.content_padding.y.lrint;
+			auto r = cast(uint)ctx.style.combo.content_padding.y.lrint;
 
 			addLayout(new DynamicRowLayout(2, 24 + r * 2));
 			curLayout.menu = MSG_EQUIPMENT;
@@ -29,12 +29,16 @@ final class WinStatus : GUIWindow
 			foreach (i, ref slot; _table)
 			{
 				auto wrap = () {
-					uint k = cast(uint) i;
-					auto combo = slot.combo = new ImageCombo(curLayout);
+					uint k = cast(uint)i;
+					auto combo = new ImageCombo(curLayout);
 
 					combo.onChange = (idx) {
 						if (auto n = idx ? idx : combo.selected)
-							_table[k].items[n - 1].action;
+						{
+							itemsForSlot(&_table[k], null)[n - 1].action; //const m = _table[k].items[n - 1];
+							//RO.status.items.getIdx(m).action;
+						}
+
 						return false;
 					};
 
@@ -54,11 +58,15 @@ private:
 	{
 		if (m.equip)
 		{
-			process(m, true);
+			{
+				auto slots = slotsForItem(m);
+				slots.each!(a => a.items ~= m.idx);
+				slots.each!(a => fill(a, true));
+			}
 
-			m.onEquip.permanent(a => process(a, true));
-			m.onUnequip.permanent(a => process(a, false));
-			m.onRemove.permanent(a => process(a, false));
+			m.onEquip.permanent(a => onEquip(a, true));
+			m.onUnequip.permanent(a => onEquip(a, false));
+			m.onRemove.permanent(&onRemove);
 		}
 	}
 
@@ -66,7 +74,13 @@ private:
 	{
 		auto slots = slotsForItem(m);
 		slots.each!(a => a.items = a.items.remove!(a => a == m.idx));
-		fill(slots);
+		slots.each!(a => fill(a, false));
+	}
+
+	void onEquip(Item m, bool equip)
+	{
+		auto slots = slotsForItem(m);
+		slots.each!(a => fill(a, equip));
 	}
 
 	auto slotsForItem(Item m)
@@ -76,38 +90,59 @@ private:
 			.array;
 	}
 
-	void process(Item item, bool canEquip)
+	// void updateSlots(Item m, void delegate(Slot*) dg)
+	// {
+	// 	_table[].map!((ref a) => &a)
+	// 		.filter!(a => a.mask & m.equip)
+	// 		.each!dg;
+	// }
+
+	void fill(Slot* slot, Item unequip)
 	{
-		auto arr = _table[].map!((ref a) => &a)
-			.filter!(a => a.mask & item.equip);
+		// auto combo = slot.combo;
 
-		foreach (slot; arr)
+		// combo.clear;
+		// combo.add(slot.name, null);
+
+		// foreach (i, m; slot.items
+		// 		.map!(a => RO.status.items.getIdx(a))
+		// 		.filter!(a => !a.equip2 || a.equip2 & slot.mask)
+		// 		.enumerate)
+		// {
+		// 	auto data = m.data;
+		// 	combo.add(data.name, makeIconTex(data.res));
+
+		// 	if (canEquip && m.equip2)
+		// 		combo.selected = cast(uint) i + 1;
+		// }
+
+		auto combo = _layouts[0].childs[slot - _table.ptr];
+		auto items = itemsForSlot(slot, unequip);
+
+		combo.clear;
+		combo.add(slot.name, null);
+
+		foreach (i, m; items)
 		{
-			auto combo = slot.combo;
-			slot.items = RO.status.items.get(a => !!((a.equip2 && canEquip
-					? a.equip2 : a.equip) & slot.mask));
+			auto data = m.data;
+			combo.add(data.name, makeIconTex(data.res));
 
-			combo.clear;
-			combo.add(slot.name, null);
-
-			foreach (i, m; slot.items)
-			{
-				auto data = m.data;
-				combo.add(data.name, makeIconTex(data.res));
-
-				if (canEquip && m.equip2 & slot.mask)
-					combo.selected = cast(uint) i + 1;
-			}
+			if (canEquip && m.equip2 & slot.mask)
+				combo.selected = cast(uint)i + 1;
 		}
+	}
+
+	auto itemsForSlot(Slot* s, Item unequip)
+	{
+		return RO.status.items.get(a => !!((a is unequip || !a.equip2 ? a.equip : a.equip2) & s
+				.mask));
 	}
 
 	struct Slot
 	{
 		uint mask;
 		string name;
-		Item[] items;
-		ImageCombo combo;
 	}
 
-	Slot[10] _table;
+	static immutable Slot[10] _table;
 }
